cde83c076d73e32de146f393897189418b87322c
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-09-28 17:34:39 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+lado = int(input("Digite o lado do quadrado: "))
+area = (lado**2)
+dobro = (area * 2)
+print("A área do quadrado é: " + str(area))
+print("O dobro da área é: " + str(dobro))
